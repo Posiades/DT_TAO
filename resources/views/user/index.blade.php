684e330a78dd33b8fd3fb61a257133f9f8f9f31d
minhv11 <nguyenvanminhv11@gmail.com>
2024-07-04 21:54:38 +0700

    img

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -141,7 +141,7 @@
             <div class="swiper-slide">
               <div class="product-card position-relative">
                 <div class="image-holder">
-                  <img src="images/product-item2.jpg" alt="product-item" class="img-fluid">
+                  <img src="images/ip11.jpg" alt="product-item" class="img-fluid">
                 </div>
                 <div class="cart-concern position-absolute">
                   <div class="cart-button d-flex">
