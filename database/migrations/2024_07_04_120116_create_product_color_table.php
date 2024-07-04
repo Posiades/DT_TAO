71a1123608142883570443a207e3f5239c351713
minhv11 <nguyenvanminhv11@gmail.com>
2024-07-04 19:07:37 +0700

    table color

---
+++ database/migrations/2024_07_04_120116_create_product_color_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_color', function (Blueprint $table) {
+            $table->increments('color_id');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('product_id')->on('product'); // Đổi từ 'product' thành 'products'
+            $table->string('color_value', 100);
+            $table->string('image_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_color');
+    }
+};
