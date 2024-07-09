7a2be17a9f8671bcf19d7c606a0fbc2e21dac47a
minhv11 <nguyenvanminhv11@gmail.com>
2024-07-09 18:23:41 +0700

    fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -124,7 +124,7 @@
             <div class="swiper-slide">
               <div class="product-card position-relative">
                 <div class="image-holder">
-                  <img src="  " alt="product-item" class="img-fluid">
+                  <img src="{{asset($iphone->image_url)}}" alt="product-item" class="img-fluid">
                 </div>
                 <div class="cart-concern position-absolute">
                   <div class="cart-button d-flex">
